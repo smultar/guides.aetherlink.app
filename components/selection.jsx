d72d042332978a614ef17e324f4a5ac016c7cdd9
Smultar <Jason@smultar.com>
2022-12-03 07:52:56 -0500

    Added `Game Play` overview page.

--- components/selection.jsx
+++ components/selection.jsx
@@ -1,7 +1,22 @@
-const Selection = ({ large }) => {
 
-    
-    
-    return 
+import React from 'react';
+import Image from 'next/image';
 
-}
+const Selection = ({ icon, name, description, background, large }) => {
+    return (
+        <div className='project'>
+            { icon && <Image className='image' src={icon}height={40} width={40}></Image>}
+            <p className='title'>{name}</p>
+            <p className='title sub'>{description}</p>
+            
+            { background && 
+                <div className='background'>
+                    <Image className='background-image' src={ background ?? 'https://media.discordapp.net/attachments/965146501720440878/967325101638950952/unknown.png'} fill={true}></Image>
+                </div>
+            }
+                
+        </div>
+    );
+};
+
+export default Selection;
