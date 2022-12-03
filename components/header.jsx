bda933730879513c5aebed7ca11bff1c904059cd
Smultar <Jason@smultar.com>
2022-12-03 08:26:56 -0500

    Added `meta` and `search` data.

---
+++ components/header.jsx
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
+
+const Header = ({ description, color, large, picture }) => {
+
+    let image = 'https://guides.aetherlink.app/images/og.png';
+    let icon = '/svgs/logo.svg';
+
+    image = (picture) ? picture : image;
+
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
+
+    return (
+        <Head>
+            {/* Meta - Tab */}
+            <meta charSet='utf-8' />
+            <title>{frontMatter.title || 'Aether Guides'}</title>
+            <meta name='application-name' content='Aether Guides'/>
+            <link rel='icon' href={icon}/>
+
+            {/* Meta - Pure HTML */}
+            <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'/>
+            <meta name='url' content={`https://guildes.aetherlink.app${asPath}`}/>
+            <meta name='theme-color' content={ color ?? '#fffff'}/>
+            <meta name='title' content={frontMatter.title || 'Aether Guides'}/>
+            <meta name='description' content={ description ?? 'Aether Guides is a documentation website, where you can discover up-to-date guides and resources for Final Fantasy XIV'} />
+
+            {/* Meta - Open Graph */}
+            <meta property='og:type' content='website' />
+            <meta property='og:url' content={`https://guildes.aetherlink.app${asPath}`} />
+            <meta property='og:title' content={frontMatter.title || 'Aether Guides'} />
+            <meta property='og:description' content={ description ?? 'Aether Guides is a documentation website, where you can discover up-to-date guides and resources for Final Fantasy XIV' } />
+            <meta property='og:image' content={image}/>
+
+            {/* Meta - Twitter */}
+            { large && <meta name='twitter:card' content='summary_large_image' /> }
+            <meta property='twitter:url' content={`https://guildes.aetherlink.app${asPath}`} />
+            <meta property='twitter:title' content={frontMatter.title || 'Aether Guides'} />
+            <meta property='twitter:description' content={ description ?? 'Aether Guides is a documentation website, where you can discover up-to-date guides and resources for Final Fantasy XIV'} />
+            <meta property='twitter:image' content={image}/>
+        </Head>
+    );
+}
+
+export default Header;
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Brand from './components/modules/brand.jsx';
+import Header from './components/header.jsx';
+
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config = {
   logo: <Brand/>,
@@ -17,6 +21,47 @@ const config = {
 
   sidebar: {
     defaultMenuCollapseLevel: 0
+  },
+
+  head: () => {
+
+    let image = 'https://guides.aetherlink.app/images/og.png';
+    let icon = '/svgs/logo.svg';
+
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
+
+    return (
+      <>
+        {/* Meta - Tab */}
+        <meta charSet='utf-8' />
+        <title>{frontMatter.title || 'Aether Guides'}</title>
+        <meta name='application-name' content='Aether Guides'/>
+        <link rel='icon' href={icon}/>
+
+        {/* Meta - Pure HTML */}
+        <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'/>
+        <meta name='url' content={`https://guildes.aetherlink.app${asPath}`}/>
+        <meta name='theme-color' content={'#fffff'}/>
+        <meta name='title' content={frontMatter.title || 'Aether Guides'}/>
+        <meta name='description' content={'Aether Guides is a documentation website, where you can discover up-to-date guides and resources for Final Fantasy XIV'} />
+
+        {/* Meta - Open Graph */}
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content={`https://guildes.aetherlink.app${asPath}`} />
+        <meta property='og:title' content={frontMatter.title || 'Aether Guides'} />
+        <meta property='og:description' content={'Aether Guides is a documentation website, where you can discover up-to-date guides and resources for Final Fantasy XIV' } />
+        <meta property='og:image' content={image}/>
+
+        {/* Meta - Twitter */}
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content={`https://guildes.aetherlink.app${asPath}`} />
+        <meta property='twitter:title' content={frontMatter.title || 'Aether Guides'} />
+        <meta property='twitter:description' content={'Aether Guides is a documentation website, where you can discover up-to-date guides and resources for Final Fantasy XIV'} />
+        <meta property='twitter:image' content={image}/>
+      </>
+    )
+
   },
 
   footer: {
